3465089ae5fd5c896aea3136efbe527bc3b0fe43
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-01 15:39:55 +0200

    Initial commit

---
+++ 2/values.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =" , 1+1)
+
+    fmt.Println(true && false)
+
+    fmt.Println(true || true)
+
+    fmt.Println(!true)
+}
+
---
+++ 7/switch.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    i := 2
+    fmt.Println("write ", 1, "as ")
+    x := time.Duration(1000)
+    switch i {
+    case 1:
+        fmt.Println("one")
+    case 2:
+        fmt.Println("two")
+    case 3:
+        fmt.Println("three")
+    }
+
+    switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+        fmt.Println("it's weekend!")
+    default:
+        fmt.Println("it's not")
+    }
+
+    t := time.Now()
+
+    switch  {
+    case t.Hour() < 12:
+        fmt.Println("it's before noon")
+    default:
+        fmt.Println("it's after noon")
+    }
+}
